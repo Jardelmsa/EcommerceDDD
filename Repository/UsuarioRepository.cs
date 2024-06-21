c92efdd9db568f155425bae4a9da89909c576d73
jardelmsa <jardelmorais@hotmail.com>
2024-06-20 23:35:04 -0300

    commit inicial api

---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,71 @@
+﻿using Infra;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public class UsuarioRepository:Conexao
+    {
+        public void AddUsuario( Usuarios usuarios)
+        {
+            try
+            {
+                AbrirConexao();
+                Cmd = new SqlCommand("INSERT INTO USUARIOS ( NOME, SENHA ) VALUES (@v1, @v2)", Con);
+                Cmd.Parameters.AddWithValue("@v1", usuarios.Nome);
+                Cmd.Parameters.AddWithValue("@v2", usuarios.Senha);
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public List<Usuarios> GetAllUsers()
+        {
+            try
+            {
+                AbrirConexao();
+                Cmd = new SqlCommand("SELECT * FROM USUARIOS", Con);
+                Dr = Cmd.ExecuteReader();
+
+               
+                List<Usuarios> lista = new List<Usuarios>();
+                while (Dr.Read())
+                {
+
+                    Usuarios users = new Usuarios();
+                    users.Id = Convert.ToInt32(Dr["ID"]);
+                    users.Nome = Convert.ToString(Dr["NOME"]);
+                    users.Senha = Convert.ToString(Dr["SENHA"]);
+                    lista.Add(users);
+
+                }
+                
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            finally
+            {
+                FecharConexao();
+            }
+        }
+    }
+}
---
+++ WebAPIEcommerce/Controllers/UsuariosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase     
+    {
+
+        
+
+       [HttpPost]
+        public void CadastrarUsuarios([FromBody] Usuarios user)
+        {
+            try
+            {
+                UsuarioRepository usuarioRepository = new UsuarioRepository();
+                usuarioRepository.AddUsuario(user);
+                
+            }
+
+             
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public List<Usuarios> ListarUsuarios()
+        {
+            try
+            {
+                UsuarioRepository usuarioRepository = new UsuarioRepository();
+                var lista = usuarioRepository.GetAllUsers();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
